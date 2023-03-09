189506653020f4981e60efc2b2dc42a46cb52df8
Diego Marques <fariia_di@hotmail.com>
2023-03-08 22:09:13 -0300

    Projeto criado

---
+++ src/test/java/br/com/diegomarques/aplicacao/AplicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegomarques.aplicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
